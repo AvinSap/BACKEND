718810dfccf1b0b8f92166508f5d9c15129e4b2c
AvinSap <avinsapkota333@gmail.com>
2025-03-31 12:19:22 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Avin Sapkota");
+const express = require('express')
+const app = express()
+
+//to run this:(listen)
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');//(Express backend is running in port 3000)
+});
+
+app.get('/', (req,res) =>{
+    res.send ("Hello this is Avin's node api")//print this on the server(localhost:3000)
+});
