ce85587d4cce7feabc42043855e8289e7178966a
AvinSap <avinsapkota333@gmail.com>
2025-04-07 23:52:09 -0500

    DB Setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose');//DB connection
 const app = express()
 
 //to run this:(listen)
@@ -7,5 +8,14 @@ app.listen(3000, () => {
 });
 
 app.get('/', (req,res) =>{
-    res.send ("Hello this is Avin's node api")//print this on the server(localhost:3000)
+    res.send ("Hello this is Avin's Node.js API ")//print this on the server(localhost:3000)
+});
+
+
+mongoose.connect("mongodb+srv://avinsapkota7:j5UkzmFQoSFSSaym@backenddb.dekb0io.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+.then(() => {
+    console.log("Connected successfully to the database!");
+})
+.catch(() => {
+    console.log("Connection failed!");
 });
