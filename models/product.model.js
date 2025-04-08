6527a8fcd00742071305b4f676828b1e83b5fdfa
AvinSap <avinsapkota333@gmail.com>
2025-04-08 00:00:18 -0500

    update

---
+++ models/product.model.js
@@ -0,0 +1,29 @@
+const { default: mongoose } = require("mongoose");
+
+const ProductSchema = mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: [true, "Enter product name"],
+        },
+
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+
+        price:{
+            type: Number,
+            required: true,
+            default: 0
+        },
+
+        image: {
+            type: String,
+            required: false
+        },
+    },
+   {
+    timestamp
+   }
