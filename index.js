59cfb2e0903ad59edaa3436ce91cd6aa7fc00de3
AvinSap <avinsapkota333@gmail.com>
2025-04-27 23:57:27 -0500

    Fixes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose');//DB connection
 const Product = require('./models/product.model');//connecting the product.
-const app = express()
+const productRoute = require("./routes/product.route.js");
+const app = express();
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -13,42 +15,42 @@ app.get('/', (req,res) =>{
     res.send ("Hello this is Avin's Node.js API ")//print this on the server(localhost:3000)
 });
 
-// Creating API
-app.post("/api/products", async (req, res) => {
-    // console.log(req.body);
-    // res.send(req.body);
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
+// // Creating API
+// app.post("/api/products", async (req, res) => {
+//     // console.log(req.body);
+//     // res.send(req.body);
+//     try {
+//         const product = await Product.create(req.body);
+//         res.status(200).json(product);
         
-    } catch (error){
-        res.status(500).json({message: error.message});
-    }
-});
+//     } catch (error){
+//         res.status(500).json({message: error.message});
+//     }
+// });
 
-// TO VIEW THE API products
+// // TO VIEW THE API products
 
-app.get ('/api/products' , async (req, res) =>{
+// app.get ('/api/products' , async (req, res) =>{
 
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({message: error. message});
-    }
-});
+//     try {
+//         const products = await Product.find({});
+//         res.status(200).json(products);
+//     } catch (error) {
+//         res.status(500).json({message: error. message});
+//     }
+// });
 
-// GET SPECIFIC PRODUCT
-app.get('/api/products/:id', async(req, res) => {
-    try{
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
+// // GET SPECIFIC PRODUCT
+// app.get('/api/products/:id', async(req, res) => {
+//     try{
+//         const { id } = req.params;
+//         const product = await Product.findById(id);
+//         res.status(200).json(product);
     
-    } catch (error){
-        res.status(500).json({message: error.message});
-    }
-});
+//     } catch (error){
+//         res.status(500).json({message: error.message});
+//     }
+// });
 
 //UPDATING A PRODUCT
 app.put('/api/products/:id', async (req, res) => {
